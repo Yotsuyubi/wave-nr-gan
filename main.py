55eca072e20bafea03f88c8fe8fba27f4246aff0
wehiroi <wehiroi@WihiroinoMacBook-Air.local>
2020-08-10 16:12:14 +0900

    update

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ import numpy as np
 
 if __name__ == '__main__':
     gpus = 1 if torch.cuda.is_available() else 0
+    dev = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    model = GAN(device='cpu')
+    model = GAN(device=dev)
     trainer = pl.Trainer(
         gpus=gpus,
         max_epochs=50000,
