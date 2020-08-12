74a14c72fc85ae3833c4742390ba603f239ee436
wehiroi <wehiroi@WihiroinoMacBook-Air.local>
2020-08-12 17:08:13 +0900

    update

--- model.py
+++ model.py
@@ -188,12 +188,21 @@ class GAN(pl.LightningModule):
             fake = fake_signal + noise
             C_fake = self.D(fake).mean()
 
-            # train with ds reg
+            # train with Gn ds reg
             z_n1 = torch.rand([real.size()[0], self.latent_length]).to(self.dev)
             z_n2 = torch.rand([real.size()[0], self.latent_length]).to(self.dev)
-            ds_reg = self.calc_diversity_sensitivity_loss(self.NoiseG, z_n1, z_n2)
+            noise_sigma_1 = self.NoiseG(z_g, z_n1)
+            noise_sigma_2 = self.NoiseG(z_g, z_n2)
+            gn_ds_reg = torch.min(torch.tensor([torch.abs(noise_sigma_2 - noise_sigma_1).mean() / torch.abs(z_n2 - z_n1).mean(), 0])).to(self.dev)
 
-            C_cost = -C_fake -0.02*ds_reg +0.5*nn.MSELoss()(fake, real)
+            # train with Gx ds reg
+            z_x1 = torch.rand([real.size()[0], self.latent_length]).to(self.dev)
+            z_x2 = torch.rand([real.size()[0], self.latent_length]).to(self.dev)
+            signal1 = self.SigG(z_x1)
+            signal2 = self.SigG(z_x2)
+            gx_ds_reg = torch.min(torch.tensor([torch.abs(signal2 - signal1).mean() / torch.abs(z_x2 - z_x1).mean(), 0])).to(self.dev)
+
+            C_cost = -C_fake -0.02*gn_ds_reg -0.02*gx_ds_reg +nn.MSELoss()(fake, real)
 
             if batch_nb == 0:
                 self.plot()
@@ -228,9 +237,6 @@ class GAN(pl.LightningModule):
 
         gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean() * 10
         return gradient_penalty
-
-    def calc_diversity_sensitivity_loss(self, G, z1, z2, tau=0):
-        return torch.min(torch.tensor([torch.abs(G(z2) - G(z1)).mean() / torch.abs(z2 - z1).mean(), tau])).to(self.dev)
 
     def configure_optimizers(self):
         G_params = list(self.SigG.parameters()) + list(self.NoiseG.parameters()) + list(self.LatentExt.parameters())
